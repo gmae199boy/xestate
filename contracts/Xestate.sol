baa4a007afd4f03960db03ee9cb82684aa640b59
gmae199boy <kim88594544@gmail.com>
2020-09-08 13:46:33 +0000

    change contract

--- contracts/Xestate.sol
+++ contracts/Xestate.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.6;
+pragma experimental ABIEncoderV2;
 
 /*
   * storage variables *
@@ -26,22 +27,23 @@ contract Xestate {
         uint32 monthlyPayment;
         uint8 progress;
         uint8 roomType;
-        uint8 reported;
-        mapping (address => Review) review;
+        address[] reported; // 나중에 수정 필
+        uint[] reviewIndex;
     }
     
     // Review Structure
     struct Review {
-        // address auth;
+        address auth;
+        uint roomIndex;
         uint8 stars;
-        string reason;
+        // string reason;
     }
     
     // Lessor Structure
     struct Lessor {
         string name;
         uint32[] registRoom;
-        mapping (address => Review) review;
+        uint[] reviewIndex;
         uint8[] reported;
     }
     
@@ -52,7 +54,12 @@ contract Xestate {
     }
     
     // All Room Array
-    Room[] public rooms;
+    mapping (uint => Room) public rooms;
+    uint roomIndex;
+    
+    // All Review mapping
+    mapping (uint => Review) public reviews;
+    uint reviewIndex;
     
     // lessor Information by Address
     mapping (address => Lessor) public lessors;
@@ -60,6 +67,19 @@ contract Xestate {
     // Lessee Information by Address
     mapping (address => Lessee) public lessees;
     
+    // wallet
+    mapping (address => uint) public balanceOf;
+    
+    
+    /*
+        Token Setting
+    */
+    
+    uint initialTokenNumber;
+    uint XST;
+    
+    // orner
+    address orner;
     
     
     
@@ -78,7 +98,8 @@ contract Xestate {
     uint8 constant ROOM_PROGRESS_CONTRACTING = 2;
     uint8 constant ROOM_PROGRESS_RENTING = 4;
 
-
+    //
+    uint8 constant MAX_STARS = 5;
 
 
     
@@ -86,7 +107,16 @@ contract Xestate {
         Functions
     */
     
+    constructor(uint _initNum) public {
+        initialTokenNumber = _initNum;
+        orner = msg.sender;
+        balanceOf[msg.sender] = initialTokenNumber;
+    }
+    
+    
+    
     // 회원 가입
+    /// 회원 가입을 위한 트랜잭션 입니다.
     function Signup(uint8 _userType, string memory _userName) public returns(string memory) {
         // switch(_userType) {
         //     case LESSOR: {
@@ -103,7 +133,7 @@ contract Xestate {
             lessees[msg.sender] = _lessee;
             return "lessee signup success";
         } else if (_userType == LESSOR) {
-            Lessor memory _lessor = Lessor(_userName, new uint32[](0), new uint8[](0));
+            Lessor memory _lessor = Lessor(_userName, new uint32[](0), new uint[](0), new uint8[](0));
             lessors[msg.sender] = _lessor;
             return "lessor signup success";
         }
@@ -119,8 +149,10 @@ contract Xestate {
     // * @param {uint8}  _roomType
     // * @return {bool}  success
     // */
+    /// 매물을 등록하기 위한 트랜잭션 입니다.
     function RegistRoom(string memory _addr, uint32 _deposit, uint32 _monthly, 
-        uint8 _progress, uint8 _roomType) public returns(bool) {
+        uint8 _progress, uint8 _roomType) public returns (bool) {
+            
         // require(bytes(lessors[msg.sender].name).length != 0 , "Lessor has not exist");
         Room memory _room = Room({
             registLessor: msg.sender,
@@ -129,21 +161,56 @@ contract Xestate {
             monthlyPayment: _monthly,
             progress: _progress,
             roomType: _roomType,
-            reported: 0
+            reported: new address[](0),
+            reviewIndex: new uint[](0)
         });
-        rooms.push(_room);
+        rooms[roomIndex++] = _room;
         
         return true;
     }
 
     // 리뷰 등록
-    function RegistReview(uint _roomId, uint8 _stars, string memory _reason) public returns(bool) {
-        require(bytes(lessees[msg.sender].name).length != 0,"Lessee has not exist");
+    /// 리뷰를 등록하기 위한 트랜잭션 입니다.
+    function RegistReview(uint _roomId, uint8 _stars) public returns (bool) {
+        // require(bytes(lessees[msg.sender].name).length != 0,"Lessee has not exist");
+        require(_stars <= MAX_STARS, "star too much");
         Review memory _review = Review({
-            stars: _stars,
-            reason: _reason
+            auth: msg.sender,
+            roomIndex: _roomId,
+            stars: _stars
         });
-        rooms[_roomId].review[msg.sender] = _review;
+        
+        rooms[_roomId].reviewIndex.push(reviewIndex);
+        reviews[reviewIndex++] = _review;
+    }
+    
+    // 매물 신고 // 수정
+    function ReportRoom(uint _roomId) public returns (bool) {
+        rooms[_roomId].reported.push(msg.sender);
+    }
+    
+    // 매물 조회
+    function GetRoom(uint _roomId) public view returns (Room memory) {
+        return rooms[_roomId];
+    }
+    
+    // 리뷰 조회
+    function GetReviews(uint _roomId) public view returns (Review[] memory) {
+        Room memory _room = rooms[_roomId];
+        Review[] memory _reviews = new Review[](_room.reviewIndex.length);
+
+        for (uint i = 0; i < _room.reviewIndex.length; i++) {
+            _reviews[i] = reviews[_room.reviewIndex[i]];
+        }
+        
+        return _reviews;
+    }
+    
+    //
+    function _tokenTransfer(address _from, address _to, uint _value) private returns (bool) {
+        require(balanceOf[_from] >= _value, "your balance is too low");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
     }
     
     // // lessor 체크
