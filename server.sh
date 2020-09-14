0fed49837ebd39aeb5197265330337d980994ce3
gmae199boy <kim88594544@gmail.com>
2020-09-14 12:18:56 +0900

    rollback

--- server.sh
+++ server.sh
@@ -15,21 +15,29 @@ fi
 if [ "$PROCESS" == "up" ]; then
     touch ./api-server/.env
     echo "BASIC=\"Basic ${BASIC}\"" >> ./api-server/.env
-
-    # 배포 전 image: node:10.16.0 쓸 때
-    if [ ! -d ./api-server/node_modules ]; then
-        cd api-server
-        npm install
-    fi
-    cd ..
-    if [ ! -d ./xestate_react/node_modules ]; then
-        cd xestate_react
-        npm install
-    fi
-    cd ..
     docker-compose up -d
+    echo "$?"
     exit 0
 fi
+
+# if [ "$PROCESS" == "up" ]; then
+#     touch ./api-server/.env
+#     echo "BASIC=\"Basic ${BASIC}\"" >> ./api-server/.env
+
+#     # 배포 전 image: node:10.16.0 쓸 때
+#     if [ ! -d ./api-server/node_modules ]; then
+#         cd api-server
+#         npm install
+#     fi
+#     cd ..
+#     if [ ! -d ./xestate_react/node_modules ]; then
+#         cd xestate_react
+#         npm install
+#     fi
+#     cd ..
+#     docker-compose up -d
+#     exit 0
+# fi
 
 # RESULT=$(docker ps | grep app)
 # echo $RESULT
