83ffa9455df957578098706c799d49ddabda90c7
gmae199boy <kim88594544@gmail.com>
2020-09-14 14:04:03 +0900

    change

--- server.sh
+++ server.sh
@@ -14,6 +14,7 @@ fi
 
 if [ "$PROCESS" == "up" ]; then
     touch ./api-server/.env
+    echo "DOCKER=1" >> ./api-server/.env
     echo "BASIC=\"Basic ${BASIC}\"" >> ./api-server/.env
     docker-compose up -d
     echo "$?"
