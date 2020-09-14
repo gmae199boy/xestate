20772bf15771ff70a5ffa40b80004d6b36dd8523
gmae199boy <kim88594544@gmail.com>
2020-09-14 00:55:07 +0000

    setting shell script

--- server.sh
+++ server.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 PROCESS=$1
+BASIC=$2
+
+
 
 if [ "$PROCESS" == "down" ]; then
+    rm ./api-server/.env
     docker-compose down
     echo "$?"
     exit 0
 fi
 
 if [ "$PROCESS" == "up" ]; then
+    touch ./api-server/.env
+    echo "BASIC=${BASIC}" >> ./api-server/.env
     docker-compose up -d
     echo "$?"
     exit 0
